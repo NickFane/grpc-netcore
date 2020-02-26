ca85048c55e4221582269f838a749de940d031b2
Nick Fane <nicholas.fanedev@gmail.com>
2020-02-26 15:34:42 +1100

    01_example draft

---
+++ 01_simple-client-server/ShopperProfileClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using ShopperProfileServer;
+
+namespace ShopperProfileConsumer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().RunConsumer().Wait();
+        }
+
+        public async Task RunConsumer()
+        {
+            // macOS doesn't support ASP.NET Core gRPC with TLS. So we're serving our app on an unsecure endpoint
+            // This switch must be set before creating the GrpcChannel/HttpClient.
+            // For more information see https://docs.microsoft.com/en-us/aspnet/core/grpc/troubleshoot?view=aspnetcore-3.1
+            AppContext.SetSwitch(
+            "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            
+            // The port number(5000) must match the port of the gRPC server.
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new ShopperProfile.ShopperProfileClient(channel);
+            
+            // Our shopper repository contains details of shopper's with IDs ranging from 1-5
+            Console.WriteLine("Enter a shopperId to search for (1-5)");
+            var profile = await client.GetShopperProfileAsync(new ShopperProfileRequest() { ShopperId = Console.ReadLine() });
+
+            Console.WriteLine($"Shopper profile | Id: {profile.ShopperDetails.ShopperId} | Name: {profile.ShopperDetails.Name}");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 01_simple-client-server/ShopperProfileServer/Services/ShopperProfileService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace ShopperProfileServer
+{
+    public class ShopperProfileService : ShopperProfile.ShopperProfileBase
+    {
+        private List<ShopperDetails> _shopperProfiles => new List<ShopperDetails>()
+        {
+            new ShopperDetails() { ShopperId = "1", Name = "Luke Skywalker" },
+            new ShopperDetails() { ShopperId = "2", Name = "Ben Solo" },
+            new ShopperDetails() { ShopperId = "3", Name = "Kylo Ren" },
+            new ShopperDetails() { ShopperId = "4", Name = "Rey P." },
+        };
+
+        public ShopperProfileService()
+        {
+
+        }
+
+        public override async Task<ShopperProfileResponse> GetShopperProfile(ShopperProfileRequest request, ServerCallContext context)
+        {
+            var shopperDetails = _shopperProfiles.Single(s => s.ShopperId == request.ShopperId);
+            return new ShopperProfileResponse() { ShopperDetails = shopperDetails };
+        }
+
+    }
+}
